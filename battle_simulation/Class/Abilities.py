b4a81cf72e772f3df63e21c788e515f843141092
Manmohan <manu6520042@gmail.com>
2025-06-09 22:20:02 +0900

    chore: renamed ability_name to name

--- battle_simulation/Class/Abilities.py
+++ battle_simulation/Class/Abilities.py
@@ -1,5 +1,5 @@
 class Ability:
 
-    def __init__(self,ability_name : str) -> None:
+    def __init__(self,name : str) -> None:
 
-        self.ability_name = ability_name
+        self.name = name
